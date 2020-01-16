093d6f3a4fdebc667d26648f08b4668a319d0a14
~A <arayamajhi@endertech.com>
2020-01-16 13:57:45 -0800

    props drill, composition and context initial commit

---
+++ src/composition/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Composition() {
+  return <div>COMPOSITION</div>;
+}
---
+++ src/propsDrill/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function PropsDrill() {
+  return <div>PROPSDRILL</div>;
+}
